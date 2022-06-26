240b9aada44479226616cb8063e78eb035e33a43
Leandro Liduvino <leandro.augusto460@gmail.com>
2022-06-26 17:40:10 -0300

    feat: save favorites to local db

--- app/src/main/java/com/leandrolid/soccernews/adapter/NewsAdapter.java
+++ app/src/main/java/com/leandrolid/soccernews/adapter/NewsAdapter.java
@@ -18,9 +18,11 @@ import java.util.List;
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private final List<News> news;
+    private final NewsListener favoriteClickListener;
 
-    public NewsAdapter (List<News> news) {
+    public NewsAdapter(List<News> news, NewsListener favoriteClickListener) {
         this.news = news;
+        this.favoriteClickListener = favoriteClickListener;
     }
 
     @NonNull
@@ -36,23 +38,29 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         Context context = holder.itemView.getContext();
         News newsItem = news.get(position);
 
-        holder.binding.tvTitle.setText(newsItem.getTitle());
-        holder.binding.tvDescription.setText(newsItem.getDescription());
-        Glide.with(context).load(newsItem.getImage()).into(holder.binding.ivThumbnail);
-        Glide.with(context).load(newsItem.getImage()).circleCrop().into(holder.binding.ivAvatar);
+        holder.binding.tvTitle.setText(newsItem.title);
+        holder.binding.tvDescription.setText(newsItem.description);
+        Glide.with(context).load(newsItem.image).into(holder.binding.ivThumbnail);
+        Glide.with(context).load(newsItem.image).circleCrop().into(holder.binding.ivAvatar);
 
         holder.binding.tbOpenLink.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(newsItem.getLink()));
+            intent.setData(Uri.parse(newsItem.link));
             holder.itemView.getContext().startActivity(intent);
         });
 
         holder.binding.ibShare.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_SUBJECT, newsItem.getTitle());
-            intent.putExtra(Intent.EXTRA_TEXT, newsItem.getLink());
+            intent.putExtra(Intent.EXTRA_SUBJECT, newsItem.title);
+            intent.putExtra(Intent.EXTRA_TEXT, newsItem.link);
             holder.itemView.getContext().startActivity(Intent.createChooser(intent, "Choose one"));
+        });
+
+        holder.binding.ibFavorite.setOnClickListener(view -> {
+            newsItem.favorite = !newsItem.favorite;
+            favoriteClickListener.onClick(newsItem);
+            notifyItemChanged(position);
         });
     }
 
@@ -68,5 +76,9 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
             super(binding.getRoot());
             this.binding = binding;
         }
+    }
+
+    public interface NewsListener {
+        void onClick(News news);
     }
 }
--- app/src/main/java/com/leandrolid/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/leandrolid/soccernews/ui/news/NewsFragment.java
@@ -1,21 +1,26 @@
 package com.leandrolid.soccernews.ui.news;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
 import com.leandrolid.soccernews.adapter.NewsAdapter;
+import com.leandrolid.soccernews.data.local.SoccerNewsDb;
 import com.leandrolid.soccernews.databinding.FragmentNewsBinding;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private SoccerNewsDb db;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
@@ -23,18 +28,26 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // TODO Improve db implementation to avoid "allowMainThreadQueries"
+        db = Room
+                .databaseBuilder(requireContext(), SoccerNewsDb.class, "soccer-news")
+                .allowMainThreadQueries()
+                .build();
+
         setupRecyclerView(newsViewModel);
         return root;
     }
 
     private void setupRecyclerView(NewsViewModel newsViewModel) {
         binding.rvNewsList.setLayoutManager(new LinearLayoutManager(getContext()));
-        newsViewModel
-                .getNews()
-                .observe(
-                        getViewLifecycleOwner(),
-                        news -> binding.rvNewsList.setAdapter(new NewsAdapter(news))
-                );
+        LifecycleOwner recyclerViewOwner = getViewLifecycleOwner();
+
+        newsViewModel.getNews().observe(
+                recyclerViewOwner,
+                news -> binding.rvNewsList.setAdapter(new NewsAdapter(news, (updatedNews) -> {
+                    Log.i("favorited", updatedNews.toString());
+                    db.newsDao().save(updatedNews);
+                })));
     }
 
     @Override
